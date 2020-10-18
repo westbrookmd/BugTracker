985aeacb869a77db8ebfac3dce90745ca4c13175
westbrookmd <71563811+westbrookmd@users.noreply.github.com>
2020-10-18 15:54:30 -0400

    Querying Database
    Added in detail page that calls the model. I should be able to reuse this for future projects since it is fairly vague

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.urls import path, re_path
 
+app_name = 'articles'
 
 urlpatterns = [
-    path('', views.article_list)
+    path('', views.article_list, name="list"),
+    re_path('(?P<slug>[\w-]+)/$', views.article_detail, name="detail"),
 ]
