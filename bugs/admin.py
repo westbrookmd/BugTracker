ce28624ed801289bb3cbe2bf739731764cca3221
westbrookmd <71563811+westbrookmd@users.noreply.github.com>
2021-04-22 23:32:59 -0400

    Temporary Bugs Removal For Pull Request

--- bugs/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Bugs
-
-# Make articles viewable on Django admin/ page
-admin.site.register(Bugs)
--- bugs/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from . import models
-
-class CreateBug(forms.ModelForm):
-    class Meta:
-        model = models.Bugs
-        fields = ['title', 'body', 'slug', 'class_name', 'thumb']
-        
